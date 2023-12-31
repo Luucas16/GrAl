cbd38d9f7ad190511a85affbb3bde6a0ef67f24a
Luucas16 <98830740+Luucas16@users.noreply.github.com>
2023-12-31 18:28:30 +0100

    Add files via upload

--- proiektunizena/public/pluggin.js
+++ proiektunizena/public/pluggin.js
@@ -1,48 +1,60 @@
+console.log("loadBegin");
 dataAll = new Array();
-document.addEventListener("keyup", (event) => {
-  if (event.key == "Ctrl") {
-    console.log("Hemendik aurrera datuak gordeko dira");
-  }
 
-  let datenow = new Date(Date.now());
-  dataAll.push(
-    "Start" +
-      ":" +
-      document.title +
-      ";" +
-      window.location.hostname +
-      ";" +
-      datenow,
-    "\n"
-  );
 
-  document.addEventListener("click", (event) => {
-    let datenow = new Date(Date.now());
-    dataAll.push("Click" + event.target + ";" + datenow, "\n");
-  });
-
-  document.addEventListener("keyup", (event) => {
-    let datenow = new Date(Date.now());
+window.onload = (function (){
+  console.log("loadBegin");
     dataAll.push(
-      "KeyUp" + event.target + ";" + event.key + ";" + datenow,
-      "\n"
+      "Start" +
+        ":" +
+        document.title +
+        ";" +
+        window.location.hostname +
+        ";" +
+        Date.now() + "\n"
     );
-    if (event.key == "F8") {
-      console.log("F8 sakatuta");
-      dataAll.push("Finish" + ";" + Date.now());
-      download();
-    }
-  });
+  
+  dataAll.push("pageLoad" + ";" + window.location.href + ";" + Date.now());
+  //alert(dataAll.toString());
+})();
 
+window.onload = (function () {
+  console.log("onloadBegin");
+  console.log("onloadEnd");
+})();
+
+document.addEventListener("click", (event) => {
+  dataAll.push(
+    "Click" +
+      event.target +
+      Date.now() + "\n"
+  );
 });
 
-  function download() {
-    const link = document.createElement("a");
-    content = "froga";
-    const file = new Blob([dataAll.toString()], { type: "text/plain" });
-    link.href = URL.createObjectURL(file);
-    link.download = "adibidea.txt";
-    link.click();
-    URL.revokeObjectURL(link.href);
+document.addEventListener("keyup", (event) => {
+  dataAll.push(
+    "KeyUp" +
+      event.key +
+      ";" +
+      Date.now() + "\n"
+  );
+  if (event.key == "F8") {
+    console.log("F8 sakatuta");
+    dataAll.push("NotFinish" + ";" + Date.now());
+    download();
+    
   }
+});
+
+
+
+function download() {
+  const link = document.createElement("a");
+  content = "froga";
+  const file = new Blob([dataAll.toString()], { type: "text/plain" });
+  link.href = URL.createObjectURL(file);
+  link.download = "adibidea.txt";
+  link.click();
+  URL.revokeObjectURL(link.href);
+}
 
